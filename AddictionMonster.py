fc65d5e9e5554f8c2c0a26d7a8cddbdfb8522b82
Roshan <139107908+redhoodtxt@users.noreply.github.com>
2024-11-28 04:10:02 +0800

    added debuff logic

--- AddictionMonster.py
+++ AddictionMonster.py
@@ -7,22 +7,28 @@ class AddictionMonster:
         self.monster_hp = 100 
         self.player_hp = 100   
 
-    def battle(self, qna, debuff): ## qna and debuff can be functions that can be defined with minigame_1.py 
+    def battle(self, qna, debuff_dict,debuff): ## qna and debuff can be functions that can be defined with minigame_1.py 
         '''this function sets the battle for the specific monster
             theres a dictionary debuff and a function debuff - need to find a way to make sure they don't conflict 
         '''
-        # create separate dictionaries of the qns and answers 
         self.qns = qna[self.name]["qns_dict"]
         self.ans = qna[self.name]["ans_dict"]
         self.debuff_flag = False
-        for i in range(len(1, self.qns)+1):
-            self.question = self.qns['Q'+str(i)]
-            if self.debuff_flag:
-                self.player_ans = debuff(self.question)
+
+        for i in range(len(1, self.qns) +1):
+            self.question_no = self.qns['Q'+str(i)]
+            if debuff_dict['debuff']:
+                self.player_ans = debuff(self.question_no)
             else:
-                player_ans = input(self.question)
+                self.player_ans = input(self.question_no)
             
-
-
+            if self.player_ans.lower == self.ans[self.question_no]:
+                monster_hp -= 20
+                if monster_hp == 0:
+                    return  'yay' # battle_win('smoking_monster')
+            else:
+                player_hp -= 25
+                if player_hp == 0:
+                    return 'nay' # battle_win('smoking_monster')
 
 
