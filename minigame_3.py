4bb7e6bd32c15b43a311dcf2d6b8894ef821cd15
AkNick4725 <1009092@mymail.sutd.edu.sg>
2024-12-02 21:39:09 +0800

    Added 5 ending definitions, images yet to import

--- minigame_3.py
+++ minigame_3.py
@@ -1,5 +1,7 @@
-anxiety_level = {1:'low/nothing', 2:'some', 3:'anxious', 4:'social anxiety', 5:'full breakdown'}
+#from image import png
 
+anxiety_level = {1:'low/nothing', 2:'some', 3:'anxious', 4:'social anxiety', 5:'full breakdown'}
+current_anxiety = anxiety_level[3]
 
 
 def anxiety(key):
@@ -19,3 +21,17 @@ def ending_check(a):
         return ending_5(a)
     pass
     
+def ending_1(a):
+    return 'img_dead'
+
+def ending_2(a):
+    return 'img_isolated'
+
+def ending_3(a):
+    return 'img_struggles'
+
+def ending_4(a):
+    return 'img_fadedfeels'
+
+def ending_5(a):
+    return 'img_freedom'
