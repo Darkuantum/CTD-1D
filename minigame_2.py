36211329c5b7694a1df3be172997aa95923f6415
Darkuantum <darkuantum.coding@gmail.com>
2024-12-02 20:43:58 +0800

    Added my sequence

---
+++ minigame_2.py
@@ -0,0 +1,8 @@
+sq_nathan = {
+    1: ["blue", "sky"],
+    2: ["fear", "dark", "death"],
+    3: ["pear", "young", "dad", "ground"],
+    4: ["cat", "moon", "car", "drink", "snow"],
+    5: ["key", "dust", "drum", "jump", "bread", "grass"]
+}
+
