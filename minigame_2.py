0891e70ec3e7823a89d8932c8d588e4afeb61157
bahboom <ryankjcheong23@gmail.com>
2024-12-03 00:51:47 +0800

    time and randomiser function

--- minigame_2.py
+++ minigame_2.py
@@ -1,4 +1,9 @@
-level_1 = {
+import random 
+import time
+
+wins = 2 #reference based of nathan
+
+easy = {
     1: ["blue", "sky"],
     2: ["fear", "dark", "death"],
     3: ["pear", "young", "dad", "ground"],
@@ -6,20 +11,35 @@ level_1 = {
     5: ["key", "dust", "drum", "jump", "bread", "grass"]
 }
 
-<<<<<<< HEAD
-dd2={
+medium = {
     1: ["able","busy","elder","garlic","holy"],
     2: ["awkward","british","confused","feline","perfume"],
     3: ["burden","boolean","critic","murder","trashcan"],
     4: ["complex","shellfish","backspin","cobweb","hiccup"],
     5: ["velvet","magnet","enrich","hectic","banish"]
-     }
-=======
-level_3 = {
+}
+
+hard = {
     1: ["animal", "behavior", "confident" ], 
     2: ["accurate", "ambition", "businessman", "customer"],
     3: ["emphasis", "explorer", "forcible","fertilize", "heritage" ],
     4: ["kangaroo", "liberate", "lithograph", "luminous", "loyalty"],
-    5: ["maintenance", "nominate", "novelist", "numeric", "quintuplet"], 
-        }
->>>>>>> 6c0b9504834528f1974a5cb44a97fa2d26802b35
+    5: ["maintenance", "nominate", "novelist", "numeric", "quintuplet"]
+}
+
+
+def randomiser(difficulty):
+    current_list = difficulty[wins+1]
+    random.shuffle(current_list)
+    return current_list
+print(randomiser(easy))
+      
+def time(t):
+    t=10
+    while t:
+        mins, secs=divmod(t,60)
+        timer='{:02d}:{:02d}'.format( mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t-=1
+    return t
