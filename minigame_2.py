1310e43d1db77d11ecc28ff81cc241a894e24a1a
Redemptionneo <neospa33@gmail.com>
2024-12-02 21:00:59 +0800

    added level 3 and modified nathan's dictionary

--- minigame_2.py
+++ minigame_2.py
@@ -1,4 +1,4 @@
-sq_nathan = {
+level_1 = {
     1: ["blue", "sky"],
     2: ["fear", "dark", "death"],
     3: ["pear", "young", "dad", "ground"],
@@ -6,3 +6,10 @@ sq_nathan = {
     5: ["key", "dust", "drum", "jump", "bread", "grass"]
 }
 
+level_3 = {
+    1: ["animal", "behavior", "confident" ], 
+    2: ["accurate", "ambition", "businessman", "customer"],
+    3: ["emphasis", "explorer", "forcible","fertilize", "heritage" ],
+    4: ["kangaroo", "liberate", "lithograph", "luminous", "loyalty"],
+    5: ["maintenance", "nominate", "novelist", "numeric", "quintuplet"], 
+        }
