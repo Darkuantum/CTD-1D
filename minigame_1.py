81da7aa259e721af005e8ad3b585c60720f0d37b
Roshan <139107908+redhoodtxt@users.noreply.github.com>
2024-12-02 23:02:51 +0800

    more logic to the class file and minigame 1 file

--- minigame_1.py
+++ minigame_1.py
@@ -2,31 +2,31 @@ import AddictionMonster as am
 '''
 this portion consists of the relevant variables, data types and functions that are need for the object instantiation, 
 namely: 
+    - lives 
     - encryption dictionary 
     - question and answer (qna) dictionary 
     - debuff flag dictionary - used because the flag itself is only mutable inside of a dictionary 
     - sight debuff function 
-
 '''
 # encryption dictionary - required for the debuff function 
 encryption_dict = {'a': '@', 'b': '6', 'c': '(', 'e': '€', 'f': '£', 'g': '&', 'i': '!', 'j': '?', 'k': '<', 'l': '1', 'o': '0', 'q': '9', 's': '$', 't': '7', 'v': '^', 'x': '*', 'y': '¥', 'z': '2'}
 # question and answer dictionary with 3 levels of nesting 
 qna = {
-    "pp_monster": {
-        "qns_dict": {"Q1": "AV is good for you"},
-        "ans_dict": {"Q1": "yes"}
-    },
+    # "pp_monster": {
+    #     "qns_dict": {"Q1": "AV is good for you"},
+    #     "ans_dict": {"Q1": "yes"}
+    # },
     "smoke_monster": {
         "qns_dict": {"Q1": "Is vaping is good for you?"},
         "ans_dict": {"Q1": "no"}
     },
-    "drug_monster": {
-        "qns_dict": {"Q1": "Drugs are good for you"},
-        "ans_dict": {"Q1": "yes"}
-    },
+    # "drug_monster": {
+    #     "qns_dict": {"Q1": "Drugs are good for you"},
+    #     "ans_dict": {"Q1": "yes"}
+    # },
     "gambling_monster": {
-        "qns_dict": {"Q1": "How much do you want to gamble?"},
-        "ans_dict": {"Q1": "yes"}
+        "qns_dict": {"Q1": "How much do you want to gamble?","Q2": "How much do you want to gamble?","Q3": "How much do you want to gamble?","Q4": "How much do you want to gamble?","Q5": "How much do you want to gamble?"},
+        "ans_dict": {"Q1": "yes","Q2": "yes","Q3": "yes","Q4": "yes","Q5": "yes"}
     },
     "alcohol_monster": {
         "qns_dict": {"Q1": "Alcohol is haram"},
@@ -48,18 +48,24 @@ def sight_debuff(question):
 '''
 this portion is the instantiation of objects from the AddictionMonster class which will create a "monster" 
 for 3 main vices - alcohol, gambling and smoking.
-
 <addiction name> = new AddictionMonster()
-
 '''
 
 ### alcohol monster 
 
-alcohol =  am.AddictionMonster('alcohol_monster')
-alcohol.battle(qna, debuff_dict,sight_debuff)
 
-smoking =  am.AddictionMonster('smoke_monster')
-smoking.battle(qna, debuff_dict,sight_debuff)
+
+
 
 gambling =  am.AddictionMonster('gambling_monster')
-gambling.battle(qna, debuff_dict,sight_debuff)
+gambling_battle = gambling.battle(qna, debuff_dict,sight_debuff)
+
+if gambling_battle: 
+    alcohol =  am.AddictionMonster('alcohol_monster')
+    alcohol_battle = alcohol.battle(qna, debuff_dict,sight_debuff)
+    if alcohol_battle:
+        smoking =  am.AddictionMonster('smoke_monster')
+        smoking.battle(qna, debuff_dict,sight_debuff)
+else:
+   gambling_battle
+    
