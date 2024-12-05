f26f501c49dbada36fe065400e5b183e15d231db
Darkuantum <darkuantum.coding@gmail.com>
2024-12-05 20:28:30 +0800

    Changed the import in main.py to accomodate the change in module name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Importing all the different minigames as modules from the other python files.
 from minigame_1 import addictionGame
 from minigame_2 import alzheimerGame
-from minigame_4 import number_memory_game
+from minigame_3 import number_memory_game
 
 # Menu for the overall app, allows the user to choose which game to play, or to exit the app.
 def main():
