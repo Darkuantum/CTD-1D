bcb3915eab7170b2bef411c38dff601f6221757c
Guo_Yao <wongguoyao0730@gmail.com>
2024-12-09 14:48:14 +0800

    changed formatting

--- minigame_3.py
+++ minigame_3.py
@@ -63,7 +63,7 @@ def checkAnswer(user_answer, correct_answer) -> bool:
 
 
 # Create a countdown of t seconds and display it 
-def countdown3(t):
+def countdown3(t) -> None:
     while t:
         mins, secs = divmod(t, 60) # mins = t//60 | secs = t%60
         timer ='{:02d}:{:02d}'.format(mins, secs) # create a string containing the mins and secs, formmatted to two digits each
